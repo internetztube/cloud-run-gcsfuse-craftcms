2cb4a2f7fdddb19443d9f40d5a73bca8b7d7ad69
Frederic Koeberl <frederic.koeberl@dynatrace.com>
2021-12-18 22:01:21 +0100

    init

--- gcsfuse.sh
+++ gcsfuse.sh
@@ -1,1 +1,2 @@
-gcsfuse --foreground --debug_http --debug_gcs --debug_fuse --implicit-dirs --key-file /app/keyfile.json cloud-run-persistent-storage $MNT_DIR
+# gcsfuse --foreground --debug_http --debug_gcs --debug_fuse --implicit-dirs --key-file /app/keyfile.json cloud-run-persistent-storage $MNT_DIR
+gcsfuse --foreground --debug_http --debug_gcs --debug_fuse --implicit-dirs $DISK_BUCKET $MNT_DIR
